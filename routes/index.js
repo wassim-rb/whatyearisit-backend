3748cff7ad6d313117765e1e7c2155707544dc5f
Wassim.rb <rbilawassim@gmail.coml>
2024-04-12 15:25:19 +0200

    backend update

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//
 router.get('/year', (req, res) => {
   const date = new Date().getFullYear().toString();
-  res.json({ date : date})
+  res.json({ year : date})
 });
 
 module.exports = router;
